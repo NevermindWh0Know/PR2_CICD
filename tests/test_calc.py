b729c899150a9ccd0092eef2345556845441ba37
NevermindWh0Know <71713936+NevermindWh0Know@users.noreply.github.com>
2025-10-04 22:13:22 +0300

    Create test_calc.py

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -2,17 +2,17 @@ from calculator import add, sub, mul, div
 import pytest
 
 def test_add():
-    assert calculator.add(2, 3) == 5
+    assert add(2, 3) == 5
 
 def test_sub():
-    assert calculator.sub(5, 3) == 2
+    assert sub(5, 3) == 2
 
 def test_mul():
-    assert calculator.mul(4, 3) == 12
+    assert mul(4, 3) == 12
 
 def test_div():
-    assert calculator.div(10, 2) == 5
+    assert div(10, 2) == 5
 
 def test_div_zero():
     with pytest.raises(ZeroDivisionError):
-        calculator.div(1, 0)
+        div(1, 0)
